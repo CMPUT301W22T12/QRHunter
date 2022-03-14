a6fb51c039f4481208b688e7c8e932b1d2d47f7b
lattajadon <lattajadon@gmail.com>
2022-03-14 12:54:23 -0600

    Kashish code from Discord.

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,81 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    EditText Email, Password;
+    Button Login;
+    TextView Create;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Email = findViewById(R.id.editTextTextEmailAddress);
+        Password = findViewById(R.id.editTextTextPersonName3);
+        fAuth = FirebaseAuth.getInstance();
+        Login = findViewById(R.id.login_button);
+        Create = findViewById(R.id.create);
+
+        Login.setOnClickListener(view -> {
+            String email = Email.getText().toString().trim();
+            String password = Password.getText().toString().trim();
+
+            if(TextUtils.isEmpty(email)){
+                Email.setError("Email is required");
+                return;
+            }
+
+            if(TextUtils.isEmpty(password)){
+                Password.setError("Password is required");
+                return;
+            }
+
+            if(password.length() < 6){
+                Password.setError("Password must be at least 6 characters long");
+                return;
+            }
+
+            // Authenticate the user
+
+            fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(Login.this, "User login successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(Login.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        });
+
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -0,0 +1,123 @@
+package com.example.myapplication;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
+
+    EditText FirstName, LastName, Email, Password,Phone;
+    Button register;
+    TextView loginPrompt;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String userID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        FirstName = findViewById(R.id.editTextTextPersonName);
+        LastName = findViewById(R.id.editTextTextPersonName2);
+        Email = findViewById(R.id.editTextTextEmailAddress);
+        Password = findViewById(R.id.editTextTextPersonName3);
+        Phone = findViewById(R.id.editTextPhone);
+        register = findViewById(R.id.register_button);
+        loginPrompt = findViewById(R.id.textView3);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String fName = FirstName.getText().toString().trim();
+                String lName = LastName.getText().toString().trim();
+                String Pno = Phone.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is required");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Password.setError("Password must be at least 6 characters long");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User registration successful", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference document = fstore.collection("Users").document(userID);
+
+                            Map <String, Object> user = new HashMap<>();
+                            user.put("fName", fName);
+                            user.put("LName", lName);
+                            user.put("PhoneNo", Pno);
+                            user.put("Email", email);
+
+                            document.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "OnSuccess: User profile is created for "+ userID);
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+        loginPrompt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+}
